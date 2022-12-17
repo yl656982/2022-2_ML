2da293b663b664d6b8fd747ce77e51cb2a3c03e7
yl656982 <82827297+yl656982@users.noreply.github.com>
2022-12-17 16:57:57 +0900

    Add files via upload

--- fashion.py
+++ fashion.py
@@ -4,49 +4,43 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-print (tf.__version__)
+#fashion mnist 데이터 로드 28x28이미지 레이블0~9
+fm = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fm.load_data()
 
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-
-class_names = ['T-shirt', 'Trouser', 'Sweater', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+c_names = ['T-shirt', 'Trouser', 'Sweater', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 print(train_images.shape)
-print(train_labels)
 print(test_images.shape)
 
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
-
+#픽셀값 범위 0~1로 조정
 train_images = train_images / 255.0
 
 test_images = test_images / 255.0
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
 
+#28x28을 784 1차원으로 변환
+#은닉층 128 노드 출력층 10노드
+#완전연결
+#10개 레이블 중 하나에 속할 확률 출력
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation='relu'),
     keras.layers.Dense(10, activation='softmax')
 ])
+#옵티마이저, 손실함수, 지표 컴파일
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+#훈련
 model.fit(train_images, train_labels, epochs=5)
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
+#테스트 셋으로 모델 정확도 측정
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 print('\n테스트 정확도:', test_acc)
 
+#이미지 예측
 predictions = model.predict(test_images)
 
+#이미지 아래 예측 레이블과 정답 레이블 출력
+#일치하면 초록 불일치 빨강
 def plot_image(i, predictions_array, true_label, img):
     predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
     plt.grid(False)
@@ -61,8 +55,10 @@ def plot_image(i, predictions_array, true_label, img):
     else:
         color = 'red'
 
-    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label], 100*np.max(predictions_array), class_names[true_label]), color=color)
+    plt.xlabel("{} {:2.0f}% ({})".format(c_names[predicted_label], 100*np.max(predictions_array), c_names[true_label]), color=color)
 
+#레이블에 속할 확률 바차트로 표시
+#정답 바 초록, 오답 바 빨강, 나머지 회색
 def plot_value_array(i, predictions_array, true_label):
     predictions_array, true_label = predictions_array[i], true_label[i]
     plt.grid(False)
@@ -75,14 +71,8 @@ def plot_value_array(i, predictions_array, true_label):
     thisplot[predicted_label].set_color('red')
     thisplot[true_label].set_color('green')
     
-i = 12
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions, test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions,  test_labels)
-plt.show()
 
+#테스트 셋 처음 15개 출력
 num_rows = 5
 num_cols = 3
 num_images = num_rows*num_cols
@@ -94,3 +84,5 @@ for i in range(num_images):
     plot_value_array(i, predictions, test_labels)
 plt.show()
 
+#테스트 셋으로 측정한 정확도는 훈련 중 측정된 정확도보다 낮음. 테스트 정확도 평균 0.87 정도로 측정됨
+#바지나 셔츠, 아우터, 발목 부츠 등은 정확도가 높고 샌들과 운동화 등은 예측 정확도가 낮음. 사진 해상도가 낮고 흑백이여서 큰 틀이 비슷한 것들은 서로 다른 특성이 많이 들어나지 않아서 그런 듯 힘.
